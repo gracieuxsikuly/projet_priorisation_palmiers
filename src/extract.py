cc7efe9f5b2a475c8360bf69fbc12043a2bbbb3b
gracieuxsikuly <graciersikuly@gmail.com>
2026-01-28 12:02:42 +0200

    ectraction done

--- src/extract.py
+++ src/extract.py
@@ -3,7 +3,7 @@ import os
 
 def extract_geojson(data_dir="../data"):
     """Lecture des fichiers GeoJSON et retour des GeoDataFrames"""
-    palmiers = gpd.read_file(os.path.join(data_dir, "palmiers.geojson"))
-    zones = gpd.read_file(os.path.join(data_dir, "zones.geojson"))
-    routes = gpd.read_file(os.path.join(data_dir, "routes.geojson"))
+    palmiers = gpd.read_file(os.path.join(data_dir, "basedadaptationgeneralemutwangamangina.geojson"))
+    zones = gpd.read_file(os.path.join(data_dir, "zones_cultures.geojson"))
+    routes = gpd.read_file(os.path.join(data_dir, "highway.geojson"))
     return palmiers, zones, routes
